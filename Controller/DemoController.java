a32abd52e6e1916b61d59a0965e21abe0abdebe0
Mahesh Anand <maheshanand1995@gmail.com>
2020-05-31 20:59:03 +0530

    Controller added;

---
+++ Controller/DemoController.java
@@ -0,0 +1,11 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+
+	public void myName() {
+		System.out.println("Hi, I'm Mahesh!");
+	}
+}
